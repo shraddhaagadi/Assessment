528a2f9be558956a55539c9c2c32aca7bce76461
shraddhaagadi <50020630+shraddhaagadi@users.noreply.github.com>
2021-02-11 14:02:21 +0100

    Update PT100_incorrect_value_no Resistor.ino

--- PT100_incorrect_value_no Resistor/PT100_incorrect_value_no Resistor.ino
+++ PT100_incorrect_value_no Resistor/PT100_incorrect_value_no Resistor.ino
@@ -1,25 +1,24 @@
-int sensorPin = A2;         // The analog pin the LM35's Vout is connected to
+int sensorPin = A2;         
 
-/*
-Initialize serial connection with the computer
-*/
+
 
 void setup()
 {
-  Serial.begin(9600);       //Begin serial connection with computer
+  Serial.begin(9600);       
   pinMode(sensorPin, INPUT);
 }
 
 void loop()
 {
-int reading = analogRead(sensorPin);   //read data from LM35 using A0 pin
+int reading = analogRead(sensorPin);   
 float voltage = reading *5.0;     //convert sensor data to voltage
 voltage /= 1024.0;
 Serial.print(voltage);
 Serial.println("volts");  //print voltage on serial monitor
 
 float temperatureC=(voltage-0.5)*100;    // convert voltage to temp
-Serial.print(temperatureC);Serial.println("degrees C"); //print temp to serial monitor
+Serial.print(temperatureC);
+Serial.println("degrees C"); //print temp to serial monitor
 
 // if(temperatureC >XXXX){
 //    digitalWrite(ledPin, HIGH);
@@ -31,7 +30,7 @@ Serial.print(temperatureC);Serial.println("degrees C"); //print temp to serial m
 //    digitalWrite(ledPin, Low);
 // }
 
-delay(1000);          // print every second
+delay(1000);         
 
 
 
